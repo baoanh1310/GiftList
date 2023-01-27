bc71175efd0823e5403aade0aecd194afa3e9138
baoanh1310 <dataholicguy@gmail.com>
2023-01-27 20:33:14 +0700

    build: got the toy

--- client/index.js
+++ client/index.js
@@ -6,9 +6,13 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const merkleTree = new MerkleTree(niceList);
+  const name = 'Oscar Hammes';
+  const index = niceList.findIndex(n => n === name);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: name,
+    proof: merkleTree.getProof(index)
   });
 
   console.log({ gift });
